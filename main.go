63ce49380b0e7e684bb1b6333990b14c321ad578
GKTBings <105783213+GKTBings@users.noreply.github.com>
2023-02-21 21:31:13 +0800

    Update main.go

--- main.go
+++ main.go
@@ -76,7 +76,7 @@ import (
 	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/coser"         // 三次元小姐姐
 	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/cpstory"       // cp短打
 	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/danbooru"      // DeepDanbooru二次元图标签识别
-	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/diana"         // 嘉心糖发病
+	//_ "github.com/FloatTech/ZeroBot-Plugin/plugin/diana"         // 嘉心糖发病
 	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/dress"         // 女装
 	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/drift_bottle"  // 漂流瓶
 	_ "github.com/FloatTech/ZeroBot-Plugin/plugin/emojimix"      // 合成emoji
@@ -239,7 +239,7 @@ func init() {
 
 	// 通过代码写死的方式添加主人账号
 	 sus = append(sus, 2294231866)
-	 sus = append(sus, 1593572468hjw)
+	 sus = append(sus, "1593572468hjw")
 
 	if *runcfg != "" {
 		f, err := os.Open(*runcfg)
